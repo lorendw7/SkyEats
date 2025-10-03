e16d9209a60ccdf72c31265291a78f2280921539
lorendw7 <122377808+lorendw7@users.noreply.github.com>
2025-10-03 11:22:33 +0800

    完成初始化环境设置

--- backend/sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
+++ backend/sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -1,6 +1,8 @@
 package com.sky;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -9,6 +11,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
 public class SkyApplication {
+    private static final Logger log = LoggerFactory.getLogger(SkyApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
